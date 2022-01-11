b99deac2c05024802110c38254a155b95b39ce1b
girobusan <girobusan@gmail>
2022-01-11 09:25:50 +0300

    +ability to read TOML config

--- src/index.js
+++ src/index.js
@@ -1,21 +1,44 @@
-
 import * as lserv from "./latid-server";
+const TOML = require('@iarna/toml');
 const ver = VERSION;
 const open = require('open');
 const fs = require('fs');
 const path = require("path");
 var argv = require('minimist')(process.argv.slice(2));
 var portset = 9999;
-//laod settings
+//load settings
 try{
-  var settings = fs.readFileSync(
-    path.join("_config" , "settings.json"),
+  var settings = TOML.parse( fs.readFileSync(
+    path.join("_config" , "settings.toml"),
     {encoding: 'utf8'}
-  )
-  portset = JSON.parse(settings).editor.local_server_port;
+  ) );
+  console.log('Read settings.toml');
+
+
 }catch{
-  console.log("No setting in settings file");
+  try{
+    settings = JSON.parse( fs.readFileSync(
+      path.join("_config" , "settings.json"),
+      {encoding: 'utf8'}
+    ) )
+    console.log('Read settings.json');
+    console.info("Looks like you're using settings.json file.")
+    console.info("Consider to convert it to TOML format (see the docs).")
+  }catch{
+    throw("No settings file");
+  }
 }
+
+console.log(settings);
+console.log(settings.editor.local_server_port);
+
+var portsetconf = settings.editor.local_server_port || "";
+console.log("Port is" , portset)
+if(portsetconf.match(/^\d\d+$/)){
+  portset = parseInt(portsetconf);
+}
+
+
 //var port = argv.p || 9999;
 //var site_dir = argv.d || process.cwd();
 lserv.configure({
@@ -29,4 +52,4 @@ if(argv.b){
 
 
 
- 
+
